9c8d8280774209f1a9ec3a30ada2e77caac52daf
Banghyungjin <banghyungjin@gmail.com>
2020-07-11 13:50:22 +0900

    app 1차 수정

--- Application/Steer_The_Wheel/app/src/main/java/com/example/steer_the_wheel/MainActivity.java
+++ Application/Steer_The_Wheel/app/src/main/java/com/example/steer_the_wheel/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.steer_the_wheel;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
@@ -12,7 +11,6 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements SensorEventListener {
-
 
     int accelXValue;
     int accelYValue;
@@ -38,7 +36,6 @@ public class MainActivity extends Activity implements SensorEventListener {
         //엑셀러로미터 센서(가속)
         accSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-
     }
     //정확도에 대한 메소드 호출 (사용안함)
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -49,9 +46,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     //센서값 얻어오기
     public void onSensorChanged(SensorEvent event) {
         Sensor sensor = event.sensor;
-        TextView textView1 = (TextView) findViewById(R.id.angleX) ;
-        TextView textView2 = (TextView) findViewById(R.id.angleY) ;
-        TextView textView3 = (TextView) findViewById(R.id.angleZ) ;
+        TextView textView1 = (TextView) findViewById(R.id.angleY) ;
         if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
             gyroX = Math.round(event.values[0] * 1000);
             gyroY = Math.round(event.values[1] * 1000);
@@ -59,30 +54,17 @@ public class MainActivity extends Activity implements SensorEventListener {
             //System.out.println("gyroX ="+gyroX);
             //System.out.println("gyroY ="+gyroY);
             //System.out.println("gyroZ ="+gyroZ);
-
-
-
         }
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             accelXValue = (int) event.values[0];
-        accelYValue = (int) event.values[1];
-        accelZValue = (int) event.values[2];
+            accelYValue = (int) event.values[1];
+            accelZValue = (int) event.values[2];
+        }
         //System.out.println("accelXValue="+accelXValue);
         //System.out.println("accelYValue="+accelYValue);
         //System.out.println("accelZValue="+accelZValue);
-        textView1.setText("Angle X : "+accelXValue) ;
-
-        textView2.setText("Angle Y : "+accelYValue) ;
-
-        textView3.setText("Angle Z : "+accelZValue) ;
+        textView1.setText("Angle Y : "+accelYValue) ;
     }
-
-    // 주기 설명
-    // SENSOR_DELAY_UI 갱신에 필요한 정도 주기
-    // SENSOR_DELAY_NORMAL 화면 방향 전환 등의 일상적인  주기
-    // SENSOR_DELAY_GAME 게임에 적합한 주기
-    // SENSOR_DELAY_FASTEST 최대한의 빠른 주기
-
 
     //리스너 등록
     protected void onResume() {
