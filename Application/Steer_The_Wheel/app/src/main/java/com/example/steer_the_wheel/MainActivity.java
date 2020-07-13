7d63b24a904c41af76709049b7ad4f7877e65283
Banghyungjin <37135305+Banghyungjin@users.noreply.github.com>
2020-07-13 15:05:12 +0900

    2020_07_13_1504_app
    앱 기본 레이아웃 추가 블루투스 부분 추가해야 함

--- Application/Steer_The_Wheel/app/src/main/java/com/example/steer_the_wheel/MainActivity.java
+++ Application/Steer_The_Wheel/app/src/main/java/com/example/steer_the_wheel/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.steer_the_wheel;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
@@ -23,7 +42,21 @@ public class MainActivity extends Activity implements SensorEventListener {
     private SensorManager mSensorManager;
     private Sensor mGyroscope;
     private Sensor accSensor;
+    SeekBar brake;
+    SeekBar accel;
+    BluetoothAdapter mBluetoothAdapter;
+    Set<BluetoothDevice> mPairedDevices;
+    List<String> mListPairedDevices;
 
+    Handler mBluetoothHandler;
+    ConnectedBluetoothThread mThreadConnectedBluetooth;
+
+    final static int BT_REQUEST_ENABLE = 1;
+    final static int BT_MESSAGE_READ = 2;
+    final static int BT_CONNECTING_STATUS = 3;
+    final static UUID BT_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +68,45 @@ public class MainActivity extends Activity implements SensorEventListener {
         mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         //엑셀러로미터 센서(가속)
         accSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        brake = (SeekBar)findViewById(R.id.seekBarB);
+        accel = (SeekBar)findViewById(R.id.seekBarA);
+
+        brake.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                TextView tv1 = (TextView)findViewById(R.id.Brake_text);
+                tv1.setText("Brake : " + String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        accel.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                TextView tv2 = (TextView)findViewById(R.id.Accel_text);
+                tv2.setText("Accel : " + String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
 
     }
     //정확도에 대한 메소드 호출 (사용안함)
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
-
 
     //센서값 얻어오기
     public void onSensorChanged(SensorEvent event) {
@@ -63,7 +128,10 @@ public class MainActivity extends Activity implements SensorEventListener {
         //System.out.println("accelXValue="+accelXValue);
         //System.out.println("accelYValue="+accelYValue);
         //System.out.println("accelZValue="+accelZValue);
-        textView1.setText("Angle Y : "+accelYValue) ;
+        textView1.setText("Angle Y : "+accelYValue);
+        if(mThreadConnectedBluetooth != null) {
+            mThreadConnectedBluetooth.write(textView1.getText().toString());
+        }
     }
 
     //리스너 등록
@@ -77,5 +145,60 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(this);
+    }
+
+    private class ConnectedBluetoothThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedBluetoothThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), "소켓 연결 중 오류가 발생했습니다.", Toast.LENGTH_LONG).show();
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while (true) {
+                try {
+                    bytes = mmInStream.available();
+                    if (bytes != 0) {
+                        SystemClock.sleep(100);
+                        bytes = mmInStream.available();
+                        bytes = mmInStream.read(buffer, 0, bytes);
+                        mBluetoothHandler.obtainMessage(BT_MESSAGE_READ, bytes, -1, buffer).sendToTarget();
+                    }
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+        public void write(String str) {
+            byte[] bytes = str.getBytes();
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), "데이터 전송 중 오류가 발생했습니다.", Toast.LENGTH_LONG).show();
+            }
+        }
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), "소켓 해제 중 오류가 발생했습니다.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
