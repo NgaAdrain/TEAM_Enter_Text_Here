8305debd3e262eeef885a1d1b53a4d9bb46c1d1a
Banghyungjin <37135305+Banghyungjin@users.noreply.github.com>
2020-07-15 16:26:56 +0900

    ㅇㅇ
    ㅇㅇ

--- Log_converter/demo.py
+++ Log_converter/demo.py
@@ -88,8 +88,6 @@ def save(fname, data, index):
             dlist[-1] = 0
             cnt1 = 0
         """
-
-
         dlist[5] = round(float(dlist[5]) * 450, 0)
         dlist[6] = float(dlist[6]) * 100 #accel
         dlist[7] = float(dlist[7]) * 100 #brake
@@ -97,6 +95,8 @@ def save(fname, data, index):
         dlist[3] = round(float(dlist[3]), 6)
         dlist[4] = round(float(dlist[4]), 6)
         dlist[-1] = 0
+        if float(dlist[1]) == 0:
+            dlist[-1] = 5
         if float(dlist[8]) == 1:
             dlist[-1] = 3
             cnt1 = 10
@@ -117,7 +117,7 @@ def save(fname, data, index):
         elif float(dlist[5]) < -1 * angle_limit:
             dlist[-1] = 2
             cnt1 = 0
-
+        
         """
         if cnt1 == 0:
             if float(dlist[5]) > angle_limit:
