46a3944ac885d8b8d0fa60d6cf3243e34dc84690
Banghyungjin <37135305+Banghyungjin@users.noreply.github.com>
2020-07-15 15:08:44 +0900

    2020_07_15_demo.py
    라벨링 변경(차선변경, 코너링 분류 일정부분 가능)

--- Log_converter/demo.py
+++ Log_converter/demo.py
@@ -13,7 +13,7 @@ OUT_LOCATION = r'.\csv_data'
 # )
 TARGET_FILE = [file for file in os.listdir(TARGET_LOCATION) if file.endswith('.dat')]
 
-DATA_TYPE = 'Timestamp,RPM,Velocity,Steering_wheel_x,Accelerator,Brake,Winker(left),Winker(right),Label1,Label2'
+DATA_TYPE = 'Timestamp,Velocity,accel_X,accel_Y,accel_Z,Steering_wheel_x,Accelerator,Brake,Winker(left),Winker(right),Label1'
 
 
 def save(fname, data, index):
@@ -26,6 +26,8 @@ def save(fname, data, index):
     left_most = 0
     right_most = 0
     line_cnt = 0
+    label = 0
+    angle_limit = 8
 
     # list name
     element_type = str(DATA_TYPE).split(',')
@@ -35,20 +37,37 @@ def save(fname, data, index):
 
     for element in data:
         dlist = str(element).split(',')
-        del dlist[19]
-        del dlist[15]
-        del dlist[14]
-        del dlist[13]
-        del dlist[12]
-        del dlist[11]
-        del dlist[10]
-        del dlist[9]
-        del dlist[8]
-        del dlist[7]
-        del dlist[5]
-        del dlist[4]
-        del dlist[2]
-        del dlist[1]
+        dlist[6] = round(float(dlist[6]), 0)
+        """
+        del dlist[21] #winker right
+        del dlist[20] #winker left
+        """
+        del dlist[19] #clutch
+        """
+        del dlist[18] #brake
+        del dlist[17] #accelerator
+        del dlist[16] #steering x #나중에 -1 추가 예정
+        """
+        del dlist[15] #z
+        del dlist[14] #y
+        del dlist[13] #rotation x
+        del dlist[12] #z
+        del dlist[11] #y
+        del dlist[10] #coordinate x
+        """
+        del dlist[9] #z
+        del dlist[8] #y
+        del dlist[7] #acceleration x
+        del dlist[6] #velocity
+        """
+        del dlist[5] #avg_fuel_consumption
+        del dlist[4] #fuel_level
+        del dlist[3] #rpm
+        del dlist[2] #gear
+        del dlist[1] #odometer
+        """
+        del dlist[0] #timestamp
+        """
         """
         if float(dlist[6]) == 1:
             dlist[-1] = 1
@@ -69,42 +88,118 @@ def save(fname, data, index):
             dlist[-1] = 0
             cnt1 = 0
         """
+        dlist[5] = round(float(dlist[5]) * 450, 0)
+        dlist[6] = float(dlist[6]) * 100 #accel
+        dlist[7] = float(dlist[7]) * 100 #brake
+        dlist[2] = round(float(dlist[2]), 6)
+        dlist[3] = round(float(dlist[3]), 6)
+        dlist[4] = round(float(dlist[4]), 6)
         dlist[-1] = 0
-        if float(dlist[3]) > 0.01:
-            dlist[-1] = 1
-            if float(dlist[3]) > cnt1:
-                left_most = float(dlist[3])
-            if cnt2 == 0:
-                cnt2 = 1
-            elif cnt2 == 1:
-                cnt2 = 2
 
-        elif float(dlist[3]) < -0.01:
-            dlist[-1] = 2
-            if float(dlist[3]) < cnt1:
-                right_most = float(dlist[3])
-            if cnt2 == 0:
-                cnt2 = 1
-            elif cnt2 == 1:
-                cnt2 = 2
-        if cnt2 == 2:
-            if left_most > right_most * 0.7 and dlist[-1] == 2:
-                dlist[-1] = 500
-            elif right_most > left_most * 0.7 and dlist[-1] == 1:
-                dlist[-1] = 400
+        if cnt1 == 0:
+            if float(dlist[5]) > angle_limit:
+                label = 1
+                cnt1 = 11
+                if float(dlist[5]) > left_most:
+                    left_most = float(dlist[5])
+            elif float(dlist[5]) < -1 * angle_limit:
+                label = 2
+                cnt1 = 21
+                if float(dlist[5]) < right_most:
+                    right_most = float(dlist[5])
+            dlist[-1] = label
+        if cnt1 == 11:
+            if float(dlist[5]) > angle_limit:
+                label = 1
+                if float(dlist[5]) > left_most:
+                    left_most = float(dlist[5])
+            else:
+                cnt1 = 12
+            dlist[-1] = label
+        if cnt1 == 12:
+            cnt2 += 1
+            label = 1
+            if float(dlist[5]) > angle_limit:
+                cnt1 = 11
+                cnt2 = 0
+            elif float(dlist[5]) < -1 * angle_limit:
+                cnt1 = 13
+                cnt2 = 0
+            if cnt2 > 30:
+                label = 0
+                cnt1 = 0
+                cnt2 = 0
+                left_most = 0
+                right_most = 0
+            dlist[-1] = label
+        if cnt1 == 13:
+            label = 1
+            if float(dlist[5]) < -1 * angle_limit:
+                if float(dlist[5]) < right_most:
+                    right_most = float(dlist[5])
+            else:
+                cnt1 = 14
+            dlist[-1] = label
+        if cnt1 == 14:
+            if right_most * (-1) > 0.5 * left_most:
+                dlist[-1] = 100
+            label = 0
+            cnt1 = 0
             left_most = 0
             right_most = 0
-        cnt2 = 0
-        if float(dlist[4])>0:
-            if float(dlist[5])>0:
+        if cnt1 == 21:
+            if float(dlist[5]) < -1 * angle_limit:
+                label = 2
+                if float(dlist[5]) < right_most:
+                    right_most = float(dlist[5])
+            else:
+                cnt1 = 22
+            dlist[-1] = label
+        if cnt1 == 22:
+            cnt2 += 1
+            label = 2
+            if float(dlist[5]) < -1 * angle_limit:
+                cnt1 = 21
+                cnt2 = 0
+            elif float(dlist[5]) > angle_limit:
+                cnt1 = 23
+                cnt2 = 0
+            if cnt2 > 30:
+                label = 0
+                cnt1 = 0
+                cnt2 = 0
+                left_most = 0
+                right_most = 0
+            dlist[-1] = label
+        if cnt1 == 23:
+            label = 2
+            if float(dlist[5]) > angle_limit:
+                if float(dlist[5]) > left_most:
+                    left_most = float(dlist[5])
+            else:
+                cnt1 = 24
+            dlist[-1] = label
+        if cnt1 == 24:
+            if left_most > (-0.5) * right_most:
+                dlist[-1] = 200
+            label = 0
+            cnt1 = 0
+            left_most = 0
+            right_most = 0
+
+
+        """
+        if float(dlist[6])>0:
+            if float(dlist[7])>0:
                 dlist.append(3)
             else:
                 dlist.append(1)
-        elif float(dlist[5])>0:
+        elif float(dlist[7])>0:
             dlist.append(2)
         else:
             dlist.append(0)
-        dlist[3] = round(float(dlist[3]),4)
+        """
+        dlist[5] = round(float(dlist[5]),4)
 
         wr.writerow(dlist)
 
@@ -127,4 +222,3 @@ if __name__ == '__main__':
         curr_file = os.path.join(TARGET_LOCATION, fname)
         data = read(curr_file)
         save(fname, data, index)
-
