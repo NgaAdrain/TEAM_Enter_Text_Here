e3a836899c0a0138e2681428ed8089e1276b11b4
Banghyungjin <37135305+Banghyungjin@users.noreply.github.com>
2020-07-17 15:28:08 +0900

    Update demo.py

--- Log_converter/demo.py
+++ Log_converter/demo.py
@@ -1,22 +1,17 @@
 import csv
 import os, sys
 import datetime
-from datetime import timedelta
 from loader.ets2data import Ets2Data
 from loader import Simlog_pb2
 
 TARGET_LOCATION = r'.\log_data'
 OUT_LOCATION = r'.\csv_data'
-
-# TARGET_FILE = (
-#     r'20181129_150355.dat',
-#     r'o_20181129_150355.dat'
-# )
 TARGET_FILE = [file for file in os.listdir(TARGET_LOCATION) if file.endswith('.dat')]
 
-DATA_TYPE = 'Timestamp,Velocity,accel_X,accel_Z,Steering_wheel_x,Accelerator,Brake,Winker(left),Winker(right),Label,Timecheck'
+DATA_TYPE = 'Timestamp,Velocity,accel_X,accel_Z,Steering_wheel_x,' \
+            'Accelerator,Brake,Winker(left),Winker(right),Label,Timecheck'
 
-#CSV 데이터 열
+# CSV 데이터 열
 TIMESTAMP = 0
 VELOCITY = 1
 ACCELX = 2
@@ -27,6 +22,7 @@ BRAKE = 6
 W_LEFT = 7
 W_RIGHT = 8
 TIMECHECK = 9
+
 
 def save(fname, data, index):
     out_name = fname[:-4] + '.csv'
@@ -48,46 +44,47 @@ def save(fname, data, index):
 
     for element in data:
         dlist = str(element).split(',')
-        dlist[6] = round(float(dlist[6]), 0)
+
+        # CSV 데이터 쓸모없는거 삭제
         """
         del dlist[21] #winker right
         del dlist[20] #winker left
         """
-        del dlist[19] #clutch
+        del dlist[19]  # clutch
         """
         del dlist[18] #brake
         del dlist[17] #accelerator
         del dlist[16] #steering x #나중에 -1 추가 예정
         """
-        del dlist[15] #z
-        del dlist[14] #y
-        del dlist[13] #rotation x
-        del dlist[12] #z
-        del dlist[11] #y
-        del dlist[10] #coordinate x
+        del dlist[15]  # z
+        del dlist[14]  # y
+        del dlist[13]  # rotation x
+        del dlist[12]  # z
+        del dlist[11]  # y
+        del dlist[10]  # coordinate x
         """
         del dlist[9] #z
         """
-        del dlist[8] #y
+        del dlist[8]  # y
         """
         del dlist[7] #acceleration x
         del dlist[6] #velocity
         """
-        del dlist[5] #avg_fuel_consumption
-        del dlist[4] #fuel_level
-        del dlist[3] #rpm
-        del dlist[2] #gear
-        del dlist[1] #odometer
+        del dlist[5]  # avg_fuel_consumption
+        del dlist[4]  # fuel_level
+        del dlist[3]  # rpm
+        del dlist[2]  # gear
+        del dlist[1]  # odometer
 
         dlist[STEERING] = round(float(dlist[STEERING]) * 450, 0)
-        dlist[ACCEL] = round(float(dlist[ACCEL]) * 100, 0) #accel
-        dlist[BRAKE] = round(float(dlist[BRAKE]) * 100, 0) #brake
+        dlist[ACCEL] = round(float(dlist[ACCEL]) * 100, 0)  # accel
+        dlist[BRAKE] = round(float(dlist[BRAKE]) * 100, 0)  # brake
         dlist[ACCELX] = round(float(dlist[ACCELX]), 6)
         dlist[ACCELZ] = round(float(dlist[ACCELZ]), 6)
 
         dlist[-1] = 0
 
-        #기초 Labeling
+        # 기초 Labeling
         if float(dlist[W_LEFT]) == 1:
             dlist[-1] = 3
             cnt1 = 10
@@ -112,15 +109,17 @@ def save(fname, data, index):
             if float(dlist[ACCEL]) < 5:
                 dlist[-1] = 7
 
-        #dlist[4] = round(float(dlist[4]),4)
+        # dlist[4] = round(float(dlist[4]),4)
 
-        #Timecheck 추가
-        prev_time = dlist[TIMESTAMP][0:4] + "-" + dlist[TIMESTAMP][4:6] + "-" + dlist[TIMESTAMP][6:8] + " " + dlist[TIMESTAMP][9:11] +\
+        # Timecheck 추가
+        prev_time = dlist[TIMESTAMP][0:4] + "-" + dlist[TIMESTAMP][4:6] + "-" + dlist[TIMESTAMP][6:8] + " " + dlist[
+                                                                                                                  TIMESTAMP][
+                                                                                                              9:11] + \
                     ":" + dlist[TIMESTAMP][11:13] + ":" + dlist[TIMESTAMP][13:15]
         if time_cnt == 0:
-            start_time = datetime.datetime.strptime(prev_time,'%Y-%m-%d %H:%M:%S')
+            start_time = datetime.datetime.strptime(prev_time, '%Y-%m-%d %H:%M:%S')
             time_cnt += 1
-        delta_time = datetime.datetime.strptime(prev_time,'%Y-%m-%d %H:%M:%S') - start_time
+        delta_time = datetime.datetime.strptime(prev_time, '%Y-%m-%d %H:%M:%S') - start_time
         dlist.append(delta_time)
 
         wr.writerow(dlist)
